9b984beec57ccca38acd21d8cdae4c9a1d2bacae
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-28 08:55:37 +0700

    Hello World, PHP Composer

---
+++ 4-hello-world/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arief-karditya/hello-world',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cb9c9356aa254802737608929f256afaf01de47e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arief-karditya/hello-world' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cb9c9356aa254802737608929f256afaf01de47e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
