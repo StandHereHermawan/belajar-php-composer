ba321813acbdd9e9556a8816cc8280b3d87be535
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-29 08:45:18 +0700

    Menambah Library

---
+++ 8-menambah-library/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arief-karditya/menambah-library',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2c0744ec9e54551a218f7d392113c07b4e9dcc8a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arief-karditya/menambah-library' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2c0744ec9e54551a218f7d392113c07b4e9dcc8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
